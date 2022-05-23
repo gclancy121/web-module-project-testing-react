22c77e0ec79e34f311a61b61ad0ab480e9952f40
Gage Clancy <gclancy121@gmail.com>
2022-05-23 15:31:49 -0500

    initial commit

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
+const testEpisode = {
+  id: 1,
+  name: 'test',
+  image: null,
+  season: 1,
+  number: 1,
+  summary: 'this show sucks lmao',
+  runtime: 100
+}
 
-test("renders without error", () => { });
+test("renders without error", () => { 
+  render(<Episode  episode={testEpisode}/>);
+});
 
-test("renders the summary test passed as prop", () => { });
+test("renders the summary test passed as prop", () => {
+  render(<Episode  episode={testEpisode}/>); render(<Episode  episode={testEpisode}/>)
 
-test("renders default image when image is not defined", () => { });
+  const episodeSummary = null;
+  
+ });
+
+// test("renders default image when image is not defined", () => { });
