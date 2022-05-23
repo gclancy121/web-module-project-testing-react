16eb2eddc88d1a2bf6faecffa25b647de58f0084
Gage Clancy <gclancy121@gmail.com>
2022-05-23 16:49:11 -0500

    finished

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render,  screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
 test('renders without errors with no props', async () => {
-  render(<Display />)
+  render(<Display />);
  });
 
-// test('renders Show component when the button is clicked ', () => { });
+test('renders Show component when the button is clicked ', async () => { 
+  render(<Display />);
+  const button = screen.getByRole('button'); 
+  userEvent.click(button);
 
-// test('renders show season options matching your data when the button is clicked', () => { });
+  await waitFor(() => {
+    const shows = screen.getByTestId('show-container');
+    expect(shows).toBeInTheDocument();
+  })
+
+});
+
+test('renders show season options matching your data when the button is clicked', async () => {
+  render(<Display />);
+  const button = screen.getByRole('button'); 
+  userEvent.click(button);
+
+  await waitFor(() => {
+    const seasons = screen.getAllByTestId('season-option');
+    expect(seasons).toHaveLength(4)
+  })
+
+ });
